5a4e269cec1f0d4473a14289488f39f2f026effb
Luisa <luisa-s-1996@protonmail.com>
2026-07-16 22:36:14 +0200

    implement swagger

---
+++ Source/BrewLedger/Extensions/ApiDocumentation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi;
+
+namespace BrewLedger.Extensions;
+
+public static class ApiDocumentation
+{
+  public static IServiceCollection AddApiDocumentation(this IServiceCollection services)
+  {
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+    services.AddSwaggerGen(options =>
+    {
+      options.SwaggerDoc("v1",
+        new OpenApiInfo { Version = "v1", Title = "BrewLedger", Description = "Simple coffee tracking for teams." });
+
+      var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+      options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    });
+
+    return services;
+  }
+}
--- Source/BrewLedger/Program.cs
+++ Source/BrewLedger/Program.cs
@@ -1,4 +1,6 @@
+using BrewLedger.Extensions;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BrewLedger;
 
@@ -7,9 +9,12 @@ public static class Program
   public static void Main(string[] args)
   {
     var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddApiDocumentation();
+    
     var app = builder.Build();
-
     app.MapGet("/", () => "BrewLedger is up!");
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     app.Run();
   }
