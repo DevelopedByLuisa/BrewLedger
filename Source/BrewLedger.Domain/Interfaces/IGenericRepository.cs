9b881ecc60bbef3ff2a3c3d5ef75759a8ab40f46
Luisa <luisa-s-1996@protonmail.com>
2026-07-17 14:39:29 +0200

    implement repository

---
+++ Source/BrewLedger.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using BrewLedger.Domain.Entities.Common;
+
+namespace BrewLedger.Domain.Interfaces;
+
+public interface IGenericRepository<TEntity> where TEntity : BaseEntity
+{
+  Task<IEnumerable<TEntity>> GetEntitiesAsync(CancellationToken cancellationToken);
+  Task<TEntity> GetEntityAsync(int id, CancellationToken cancellationToken);
+  Task<TEntity> CreateEntityAsync(TEntity entity, CancellationToken cancellationToken);
+}
---
+++ Source/BrewLedger.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using BrewLedger.Domain.Entities.Common;
+using BrewLedger.Domain.Interfaces;
+
+namespace BrewLedger.Infrastructure.Repositories;
+
+public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity
+{
+  public async Task<IEnumerable<TEntity>> GetEntitiesAsync(CancellationToken cancellationToken)
+  {
+    throw new System.NotImplementedException();
+  }
+
+  public async Task<TEntity> GetEntityAsync(int id, CancellationToken cancellationToken)
+  {
+    throw new System.NotImplementedException();
+  }
+
+  public async Task<TEntity> CreateEntityAsync(TEntity entity, CancellationToken cancellationToken)
+  {
+    throw new System.NotImplementedException();
+  }
+}
